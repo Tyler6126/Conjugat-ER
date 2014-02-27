de465f00a7f5149daaf2b14cde9bde465b3950b9
T B <Tyler6126@users.noreply.github.com>
2014-02-27 15:46:01 -0500

    Updated main.cpp
    + Added more comments and explanations
    ~ Updated to 'Conjugat-ER' v1.1.0

--- main.cpp
+++ main.cpp
@@ -4,15 +4,13 @@ using namespace std;
 
 int main()
 {
-	while(true)
+    while(true)
     {
-    
+    	
     	/*
-    	First 6 strings and string[] are initiated, and the string[] are given values. 
-    	The string[] stores the French pronouns and word endings used in conjugating 
-    	(First person singular, First person plural, etc.)
+    	Initiate all the strings and string[]s, and assign a value to the static arrays. This prevents a previous word from being used multiple times in the same code. Arrays store static variables such as 'Pronouns' and 'Endings', allowing the use of more compace and efficient code.
     	*/
-    
+    	
         string word;
         string wordRoot;
         string wordSuffix;
@@ -20,19 +18,24 @@ int main()
         string wordPronouns[] = {"Je ", "Tu ", "Il, Elle, On ", "Nous ", "Vous ", "Ils, Elles, Ons "};
         string wordEndings_ER[] = {"e", "es", "e", "ons", "ez", "ent"};
         string wordEndings_RE[] = {"s", "s", " ", "ons", "ez", "ent"};
+        string wordEndings_IR[] = {"is", "is", "it", "issons", "issez", "issent"};
 
+		/*
+		Gets input from the user, and stores it in the variable 'word'. The function 'getline()' is used rather than 'cin' for my prefered input method. Then, it takes the variable 'word' and splits it into 'wordRoot' and 'wordSuffix',  so the program can determine how to conjugate the word.
+		*/
+		
         cout << "Enter a word to conjugate: ";
         getline(cin, word);
 
-		/*
-		Takes the variable 'word' from the 'getline()' function and breaks it into two parts:
-		1. The word root stored by 'wordRoot;
-		2. The word suffix stored by 'wordSuffix'
-		*/
-		
         wordRoot = word.substr(0, word.length() - 2);
         wordSuffix = word.substr(word.length() - 2);
 
+		/*
+		Checks to see what main verb type the word is, by comparing the 'wordSuffix' to the type. The three types are 'er', 'ir', and 're', and are all supported. For future use, this section will change over-time, especially when the UI update comes in a month.
+		
+		Then, it sets an integer, 'x', to the values 0-6, letting it read the length of all the arrays, rather than having multiple loops. It than prints 'wordPronouns' to the value of 'x', the 'wordRoot', and 'wordEndings_TYPE' to the value of 'x'
+		*/
+		
         if(wordSuffix == "er")
         {
             for(int x = 0; x < 6; x++)
@@ -49,7 +52,10 @@ int main()
         }
         else if (wordSuffix == "ir")
         {
-            cout << "-IR verbs aren't supported yet! Sorry!" << endl;
+            for(int x = 0; x < 6; x++)
+            {
+                cout << wordPronouns[x] + wordRoot + wordEndings_IR[x] << endl;
+            }
         }
         else
         {
